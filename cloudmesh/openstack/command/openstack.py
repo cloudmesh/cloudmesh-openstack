50824be8e49c25f092e924662de2c4088e6f9534
Gregor von Laszewski <laszewski@gmail.com>
2020-10-27 13:55:02 -0400

    remove future as we switch completely to python 3

--- cloudmesh/openstack/command/openstack.py
+++ cloudmesh/openstack/command/openstack.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 from cloudmesh.common.console import Console
 from cloudmesh.common.variables import Variables
 from cloudmesh.configuration.Config import Config
